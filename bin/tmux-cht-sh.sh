0fa2f1ae0404c105afe8632342d2da08c09d0760
Kenos <flatware-gold-bronze@tutanota.com>
2023-07-04 21:32:54 -0600

    Add failsafes

--- bin/tmux-cht-sh.sh
+++ bin/tmux-cht-sh.sh
@@ -2,7 +2,19 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ITEM="$(curl -s https://cheat.sh/:list | fzf)"
+
+if [ "$ITEM" == "" ]; then
+  exit 0
+fi
+
 read -e -p "Query for $ITEM: " QUERY
+
+if [ "$QUERY" == "" ]; then
+  printf "\e[31mPlease enter a query!\e[0m\n\e[5mPress any key to exit\e[0m"
+  read -e -n 1
+  exit 0
+fi
+
 QUERY="$(printf $QUERY | sed 's/\ /+/g')"
 
 curl -s "https://cht.sh/$ITEM/$QUERY" | $PAGER
