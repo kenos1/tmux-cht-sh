15970f91fd41d512947d92ea04460552fbb68928
Kenos <flatware-gold-bronze@tutanota.com>
2023-07-06 13:44:16 -0600

    Add preview and cache list

--- bin/tmux-cht-sh.sh
+++ bin/tmux-cht-sh.sh
@@ -10,19 +10,20 @@ else
   CHTSH="$CURRENT_DIR/cht.sh"
 fi
 
-ITEM="$(curl -s https://cheat.sh/:list | fzf)"
+if test -f "$CURRENT_DIR/list"; then
+  LIST="$CURRENT_DIR/list"
+else
+  curl https://cht.sh/:list > "$CURRENT_DIR/list"
+  LIST="$CURRENT_DIR/list"
+fi
+
+ITEM="$(cat $LIST | fzf --preview="bash $CHTSH {}" )"
 
 if [ "$ITEM" == "" ]; then
   exit 0
 fi
 
 read -e -p "Query for $ITEM: " QUERY
-
-if [ "$QUERY" == "" ]; then
-  printf "\e[31mPlease enter a query!\e[0m\n\e[5mPress any key to exit\e[0m"
-  read -e -n 1
-  exit 0
-fi
 
 QUERY="$(printf $QUERY | sed 's/\ /+/g')"
 
